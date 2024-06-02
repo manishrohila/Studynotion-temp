d08dd0b5415f82b70f9f9594d24f0e62a1d01f52
manish <apple@Apples-MacBook-Air.local>
2024-06-02 16:05:53 +0530

    feat: Catalog page added

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -9,18 +9,8 @@ import ProfileDropDown from '../core/Auth/ProfileDropDown'
 import { apiConnector } from '../../services/apiconnector'
 import { categories } from '../../services/apis'
 import { useState } from 'react'
-import {IoIosArrowDropdownCircle} from "react-icons/io"
+import { BsChevronDown } from "react-icons/bs"
 
-const subLinks = [
-    {
-        title: "python",
-        link:"/catalog/python"
-    },
-    {
-        title: "web dev",
-        link:"/catalog/web-development"
-    },
-];
 
 
 const Navbar = () => {
@@ -28,15 +18,16 @@ const Navbar = () => {
     const {token} = useSelector( (state) => state.auth );
     const {user} = useSelector( (state) => state.profile );
     const {totalItems} = useSelector( (state) => state.cart )
+    const [loading, setLoading] = useState(false)
     const location = useLocation();
 
-    const [ssubLinks, setSsubLinks]  = useState([]);
+    const [subLinks, setSubLinks]  = useState([]);
 
     const fetchSublinks = async() => {
         try{
             const result = await apiConnector("GET", categories.CATEGORIES_API);
             console.log("Printing Sublinks result:" , result);
-            setSsubLinks(result.data.data);
+            setSubLinks(result.data.data);
         }
         catch(error) {
             console.log("Could not fetch the category list");
@@ -63,59 +54,64 @@ const Navbar = () => {
       </Link>
 
       {/* Nav Links */}
-      <nav>
-        <ul className='flex gap-x-6 text-richblack-25'>
-        {
-            NavbarLinks.map( (link, index) => (
-                 <li key={index}>
-                    {
-                        link.title === "Catalog" ? (
-                            <div className='relative flex items-center gap-2 group'>
-                                <p>{link.title}</p>
-                                <IoIosArrowDropdownCircle/>
-
-                                <div className='invisible absolute left-[50%]
-                                    translate-x-[-50%] translate-y-[80%]
-                                 top-[50%]
-                                flex flex-col rounded-md bg-richblack-5 p-4 text-richblack-900
-                                opacity-0 transition-all duration-200 group-hover:visible
-                                group-hover:opacity-100 lg:w-[300px]'>
-
-                                <div className='absolute left-[50%] top-0
-                                translate-x-[80%]
-                                translate-y-[-45%] h-6 w-6 rotate-45 rounded bg-richblack-5'>
-                                </div>
-
-                                {
-                                    subLinks.length ? (
-                                            subLinks.map( (subLink, index) => (
-                                                <Link to={`${subLink.link}`} key={index}>
-                                                    <p>{subLink.title}</p>
-                                                </Link>
-                                            ) )
-                                    ) : (<div></div>)
-                                }
-
-                                </div>
-
-
-                            </div>
-
+      <nav className="hidden md:block">
+          <ul className="flex gap-x-6 text-richblack-25">
+            {NavbarLinks.map((link, index) => (
+              <li key={index}>
+                {link.title === "Catalog" ? (
+                  <>
+                    <div
+                      className={`group relative flex cursor-pointer items-center gap-1 ${
+                        matchRoute("/catalog/:catalogName")
+                          ? "text-yellow-25"
+                          : "text-richblack-25"
+                      }`}
+                    >
+                      <p>{link.title}</p>
+                      <BsChevronDown />
+                      <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[140px]">
+                        <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
+                        {loading ? (
+                          <p className="text-center">Loading...</p>
+                        ) : subLinks.length ? (
+                          <>
+                            {subLinks
+                              ?.map((subLink, i) => (
+                                <Link
+                                  to={`/catalog/${subLink.name
+                                    .split(" ")
+                                    .join("-")
+                                    .toLowerCase()}`}
+                                  className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50"
+                                  key={i}
+                                >
+                                  <p>{subLink.name}</p>
+                                </Link>
+                              ))}
+                          </>
                         ) : (
-                            <Link to={link?.path}>
-                                <p className={`${ matchRoute(link?.path) ? "text-yellow-25" : "text-richblack-25"}`}>
-                                    {link.title}
-                                </p>
-                                
-                            </Link>
-                        )
-                    }
-                </li>
-             ) )
-        }
-
-        </ul>
-      </nav>
+                          <p className="text-center">No Courses Found</p>
+                        )}
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <Link to={link?.path}>
+                    <p
+                      className={`${
+                        matchRoute(link?.path)
+                          ? "text-yellow-25"
+                          : "text-richblack-25"
+                      }`}
+                    >
+                      {link.title}
+                    </p>
+                  </Link>
+                )}
+              </li>
+            ))}
+          </ul>
+        </nav>
 
 
         {/* Login/SignUp/Dashboard */}
